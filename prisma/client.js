b6be6850bebb021c81cee357a2ad7ea4949175fc
Mena <dayjacs03@gmail.com>
2026-01-17 16:48:52 -0600

    prisma schema

---
+++ prisma/client.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { Pool } = require("pg");
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+module.exports = new PrismaClient({ adapter });
