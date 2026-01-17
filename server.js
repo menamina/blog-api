94f3445000066e1f962a67e8bc90ed77e6800761
Mena <dayjacs03@gmail.com>
2026-01-17 16:51:06 -0600

    prisma schema

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session/");
 const passport = require("passport");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5555;
 const server = express();
 
 server.use(express.json());
@@ -11,6 +11,6 @@ server.listen(port, (error) => {
   if (!error) {
     console.log("active");
   } else {
-    console.log("whomp :(");
+    console.log(`whomp :( @ ${error.message}`);
   }
 });
