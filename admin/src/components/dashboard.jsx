af9d4b175f4ef0693e184d57340c297922b90c21
Mena <dayjacs03@gmail.com>
2026-01-23 13:17:29 -0600

    still rendering dashbord conditionals

--- admin/src/components/dashboard.jsx
+++ admin/src/components/dashboard.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Dashboard({
@@ -10,10 +9,17 @@ function Dashboard({
   loginErr,
   errors,
 }) {
-  function logout(e) {
+  const deletePost = () => {};
+  const openPost = () => {};
+
+  function logout() {
     // async func to logout
     setUser(null);
   }
+
+  const publishedPost = posts.filter((post) => post.published === true);
+  const unpublishedPost = posts.filter((post) => post.published === false);
+
   return (
     <div>
       {loginErr ? <div>{loginErr}</div> : null}
@@ -25,29 +31,92 @@ function Dashboard({
       {userNotAdmin === false ? (
         <div className="adminDash">
           <div className="nav">
-            <div><Link to="/new-post">NEW POST</Link></div>
+            <div>
+              <Link to="/new-post">NEW POST</Link>
+            </div>
             <div>Hi, {user.name}</div>
-            <div><Link to="/admin-login" onClick={logout}>LOGOUT</Link></div>
+            <div>
+              <Link to="/admin-login" onClick={logout}>
+                LOGOUT
+              </Link>
+            </div>
           </div>
           <div className="postContain">
             {posts.length === 0 ? (
-              <div>No posts yet. Wanna <Link to="/new-post">create one?</Link></div>
+              <div>
+                No posts yet. Wanna <Link to="/new-post">create one?</Link>
+              </div>
             ) : (
-              <div className="postGrid">
-                {posts.map((post) => (
-                  <div key={post.id || post._id || post.title}>
-                    <div className="left postDiv">
-                      <img src={`api/multerIMG/${post.img}`} alt={post.title} />
-                    </div>
-                    <div className="right postDiv">
-                      <div>{post.createdAt}</div>
-                      <div>{post.title}</div>
-                      <div>{(post.body || "").slice(0, 15)}...</div>
-                      <div className="line"></div>
-                      <div>{(post.commentsOnThisPost || []).length} comments</div>
-                    </div>
+              <div className="post">
+                <div className="postHeader">
+                  <div className="published">Published</div>
+                </div>
+                {publishedPost.length === 0 ? (
+                  <div>no published posts</div>
+                ) : (
+                  <div className="postGrid">
+                    {publishedPost.map((post) => (
+                      <div key={post.id}>
+                        <div className="left postDiv">
+                          <img
+                            src={`api/multerIMG/${post.img}`}
+                            alt={post.title}
+                          />
+                        </div>
+                        <div className="right postDiv">
+                          <div>{post.createdAt}</div>
+                          <div>{post.title}</div>
+                          <div>{(post.body || "").slice(0, 15)}...</div>
+                          <div className="line"></div>
+                          <div>
+                            {(post.commentsOnThisPost || []).length} comments
+                          </div>
+                          <div className="postActions">
+                            <div onClick={deletePost}>X</div>
+                            <div onClick={openPost}>
+                              <Link to="">edit</Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                ))}
+                )}
+
+                <div className="postHeader">
+                  <div className="draft">Drafts</div>
+                </div>
+                {unpublishedPost.length === 0 ? (
+                  <div>no draft posts</div>
+                ) : (
+                  <div className="postGrid">
+                    {unpublishedPost.map((post) => (
+                      <div key={post.id}>
+                        <div className="left postDiv">
+                          <img
+                            src={`api/multerIMG/${post.img}`}
+                            alt={post.title}
+                          />
+                        </div>
+                        <div className="right postDiv">
+                          <div>{post.createdAt}</div>
+                          <div>{post.title}</div>
+                          <div>{(post.body || "").slice(0, 15)}...</div>
+                          <div className="line"></div>
+                          <div>
+                            {(post.commentsOnThisPost || []).length} comments
+                          </div>
+                          <div className="postActions">
+                            <div onClick={deletePost}>X</div>
+                            <div onClick={openPost}>
+                              <Link to="">edit</Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
             )}
           </div>
